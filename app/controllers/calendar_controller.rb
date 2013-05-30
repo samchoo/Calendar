a01730057d5f64d35574514ae7736cba4382240f
Sam Choo <samchoo@outlook.com>
2013-05-30 09:58:31 -0500

    Calendar with events available now

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,6 +4,16 @@
  @year = (params[:year] || Time.zone.now.year).to_i
  @shown_month = Date.civil(@year, @month)
  @event_strips = Event.event_strips_for_month(@shown_month)
+
+ x = Event.new
+   x.name = "This is a test"
+   x.start_at = Time.current
+   x.end_at = Time.current
+   x.save
  end
+ end
+ 
+ def test
+   
  end
 
--- db/migrate/20130529213820_create_events.rb
+++ db/migrate/20130529213820_create_events.rb
@@ -1,6 +1,7 @@
 class CreateEvents < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
+      t.integer :id
       t.string :name
       t.datetime :start_at
       t.datetime :end_at
