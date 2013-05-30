a5f60bb6394e77048137bdf7c99581aadc15feed
Sam Choo <samchoo@outlook.com>
2013-05-30 10:49:33 -0500

    Add user log in test

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,6 +4,21 @@
  @year = (params[:year] || Time.zone.now.year).to_i
  @shown_month = Date.civil(@year, @month)
  @event_strips = Event.event_strips_for_month(@shown_month)
+
+=begin
+   x = Event.new
+   x.name = "This is a test"
+   x.start_at = Time.current
+   x.end_at = Time.current
+   x.save
+=end
+
+
+
  end
+ end
+ 
+ def test
+   
  end
 
