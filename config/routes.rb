8afeea32f65e34237ef378e94eb3782dc0b8b8e9
Luke Helmcamp <lukehelmcamp@yahoo.com>
2013-05-31 12:22:22 -0500

    Bootstrap working and Login successfully routes to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Calendar::Application.routes.draw do
   devise_for :users
-  
+  root :to => "calendar#index"
   match '/events/(/:id)' => 'calendar#events'
 
   match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
