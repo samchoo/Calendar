1a27db432e7253d2a64a33746499a0db1ab0b255
Sam Choo <samchoo@outlook.com>
2013-06-03 14:59:49 -0500

    OMG

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,7 +3,7 @@
  @month = (params[:month] || Time.zone.now.month).to_i
  @year = (params[:year] || Time.zone.now.year).to_i
  @shown_month = Date.civil(@year, @month)
- @event_strips = Event.event_strips_for_month(@shown_month)
+ @event_strips = Event.where('id' => 19).event_strips_for_month(@shown_month)
  end
    
    def events
