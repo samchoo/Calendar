ab1b116627ca7a459715c0e16d84e15d27bdf8d0
Sam Choo <samchoo@outlook.com>
2013-06-04 10:02:23 -0500

    Did not check status previously.  Now this version shows events specific to the user.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,9 +1,25 @@
  class CalendarController < ApplicationController
  def index
+ 
+ 
+ 
+ #@event = Event.new
+ #@event.name = "If my email is asdf@asdf.com, then this is correct."
+ #@event.email = "bob@bob.com"
+ #@event.start_at = Time.now
+ #@event.end_at = Time.now
+ 
+ #@event.save
+ 
+ 
+   
  @month = (params[:month] || Time.zone.now.month).to_i
  @year = (params[:year] || Time.zone.now.year).to_i
  @shown_month = Date.civil(@year, @month)
- @event_strips = Event.where('id' => 19).event_strips_for_month(@shown_month)
+ @event_strips = Event.where('email' => current_user.email).event_strips_for_month(@shown_month)
+ 
+ 
+ 
  end
    
    def events
