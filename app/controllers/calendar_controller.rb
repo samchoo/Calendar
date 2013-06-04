1d6a3c0dbc443d9cf223d58706c901e60103ed66
Sam Choo <samchoo@outlook.com>
2013-06-04 15:50:36 -0500

    Everything is working so I just thought I would make a commit just in case.  Never hurts right?

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -10,15 +10,14 @@
  #@event.end_at = Time.now
  
  #@event.save
- 
+
  
    
  @month = (params[:month] || Time.zone.now.month).to_i
  @year = (params[:year] || Time.zone.now.year).to_i
  @shown_month = Date.civil(@year, @month)
  @event_strips = Event.where('email' => current_user.email).event_strips_for_month(@shown_month)
- 
- 
+
  
  end
    
